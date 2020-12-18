2ebcd82fc2ae270c90a66015267ba046be79b483
BeastyDSK <53361004+BeastyDSK@users.noreply.github.com>
2020-12-18 20:51:47 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,77 +12,88 @@ function jahkuwbhw_wjsksa(url)
         return response.json();
     })
     .then(data => {
-        data.value.forEach(e=>
+        if(data.value.length>0)
         {
-            var mainContent = null;
-            var mainContainer = null;
-            var contentImage = null;
-            var contentDescription = null;
-            var contentName = null;
-            var textBox = null;
-            var contentUrl = null;
-            var contentDate = null;
-            
-            if(e.image && (e.image.thumbnail.width >= e.image.thumbnail.height))
+            data.value.forEach(e=>
             {
-                if(document.querySelector('.main-container'))
+                var mainContent = null;
+                var mainContainer = null;
+                var contentImage = null;
+                var contentDescription = null;
+                var contentName = null;
+                var textBox = null;
+                var contentUrl = null;
+                var contentDate = null;
+                
+                if(e.image && (e.image.thumbnail.width >= e.image.thumbnail.height))
                 {
-                    mainContainer = document.querySelector('.main-container');
+                    if(document.querySelector('.main-container'))
+                    {
+                        mainContainer = document.querySelector('.main-container');
+                    }
+                    else
+                    {
+                        var body = document.querySelector('body');
+                        mainContainer = document.createElement('main');
+                        mainContainer.setAttribute('class','main-container');
+                        body.appendChild(mainContainer);
+                    }
+                    
+                    
+                    mainContent = document.createElement('article');
+                    mainContent.setAttribute('class','main-content');
+                    
+                    contentImage = document.createElement('img');
+                    contentImage.setAttribute('class','content-img');
+                    contentImage.src = e.image.thumbnail.contentUrl+'&w=640&480&p=0';
+                    mainContent.appendChild(contentImage);
+
+                    textBox = document.createElement('div');
+                    textBox.setAttribute('class','text-box');
+            
+                    contentDate = document.createElement('span');
+                    contentDate.setAttribute('class','content-date');
+
+                    var date = new Date(e.datePublished);
+                    contentDate.innerHTML = date.toString().slice(0,15);
+                    textBox.appendChild(contentDate);
+            
+                    contentName = document.createElement('h1');
+                    contentName.setAttribute('class','content-name');
+                    contentName.innerHTML = e.name;
+                    textBox.appendChild(contentName);
+
+                    contentDescription = document.createElement('p');
+                    contentDescription.setAttribute('class','content-description');
+                    contentDescription.innerHTML = e.description;
+                    textBox.appendChild(contentDescription);
+
+                    contentUrl = document.createElement('a');
+                    contentUrl.setAttribute('class','content-url');
+                    contentUrl.href = e.url;
+                    contentUrl.target = 'blank';
+                    contentUrl.innerHTML = 'Read More';
+                    textBox.appendChild(contentUrl);
+
+                    mainContent.appendChild(textBox);
+                    
+                    if(nightmode==false) {
+                        mainContent.style.backgroundColor = 'black';
+                        mainContent.style.color = 'white';
+                    }
+                    mainContainer.appendChild(mainContent);
                 }
-                else
-                {
-                    var body = document.querySelector('body');
-                    mainContainer = document.createElement('main');
-                    mainContainer.setAttribute('class','main-container');
-                    body.appendChild(mainContainer);
-                }
-                
-                
-                mainContent = document.createElement('article');
-                mainContent.setAttribute('class','main-content');
-                
-                contentImage = document.createElement('img');
-                contentImage.setAttribute('class','content-img');
-                contentImage.src = e.image.thumbnail.contentUrl+'&w=640&480&p=0';
-                mainContent.appendChild(contentImage);
-
-                textBox = document.createElement('div');
-                textBox.setAttribute('class','text-box');
-        
-                contentDate = document.createElement('span');
-                contentDate.setAttribute('class','content-date');
-
-                var date = new Date(e.datePublished);
-                contentDate.innerHTML = date.toString().slice(0,15);
-                textBox.appendChild(contentDate);
-        
-                contentName = document.createElement('h1');
-                contentName.setAttribute('class','content-name');
-                contentName.innerHTML = e.name;
-                textBox.appendChild(contentName);
-
-                contentDescription = document.createElement('p');
-                contentDescription.setAttribute('class','content-description');
-                contentDescription.innerHTML = e.description;
-                textBox.appendChild(contentDescription);
-
-                contentUrl = document.createElement('a');
-                contentUrl.setAttribute('class','content-url');
-                contentUrl.href = e.url;
-                contentUrl.target = 'blank';
-                contentUrl.innerHTML = 'Read More';
-                textBox.appendChild(contentUrl);
-
-                mainContent.appendChild(textBox);
-                
-                if(nightmode==false) {
-                    mainContent.style.backgroundColor = 'black';
-                    mainContent.style.color = 'white';
-                }
-                mainContainer.appendChild(mainContent);
-            }
-
-        })
+            })
+        }
+        else {
+            var a =prompt("The search Term is not available Please provide any other");
+            if (a=='KrishnaPrasanth Dsk')
+            {
+                alert('Thank You 🥰🥰🥰🥰🥰🥰🥰');
+                jahkuwbhw_wjsksa("https://bing-news-search1.p.rapidapi.com/news?count=10&originalImg=true&safeSearch=Off&textFormat=Html");
+            }else
+                jahkuwbhw_wjsksa(`https://bing-news-search1.p.rapidapi.com/news/search?count=10&originalImg=true&freshness=Day&textFormat=Html&safeSearch=Off&q=${a}`);
+        }
     })
     .catch(err => {
 	    console.log(err);
@@ -100,7 +111,8 @@ var newsForm = document.querySelector('.news-form');
 newsForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     var main = document.querySelector('.main-container');
-    document.querySelector('body').removeChild(main);
+    if(main!=undefined)
+        document.querySelector('body').removeChild(main);
     var query = document.querySelector('.query');
     jahkuwbhw_wjsksa(`https://bing-news-search1.p.rapidapi.com/news/search?count=10&originalImg=true&freshness=Day&textFormat=Html&safeSearch=Off&q=${query.value}`);
 })
@@ -131,8 +143,7 @@ night.addEventListener('click',(e)=>{
             e.style.backgroundColor = 'black';
             e.style.color = 'white';
             
-        })
-        
+        }) 
     }
     else {
         icon.src = '/img/coffee-cup.svg';
@@ -148,4 +159,3 @@ night.addEventListener('click',(e)=>{
     }
     nightmode = !nightmode;
 })
-
